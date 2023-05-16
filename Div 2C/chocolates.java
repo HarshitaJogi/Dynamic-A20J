e15aba186ab95dda21b5099f0011b93bdd846738
HarshitaJogi <109593461+HarshitaJogi@users.noreply.github.com>
2023-05-16 14:14:38 +0530

    Add files via upload

---
+++ Div 2C/chocolates.java
@@ -0,0 +1,100 @@
+import java.io.*;
+import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class chocolates{
+
+	static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+  
+        public FastReader()
+        {
+            br = new BufferedReader(
+                new InputStreamReader(System.in));
+        }
+  
+        String next()
+        {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                }
+                catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+  
+        int nextInt() { return Integer.parseInt(next()); }
+  
+        long nextLong() { return Long.parseLong(next()); }
+  
+        double nextDouble()
+        {
+            return Double.parseDouble(next());
+        }
+  
+        String nextLine()
+        {
+            String str = " ";
+            StringTokenizer st = new StringTokenizer(str);
+            try {
+                if(st.hasMoreTokens()){
+                    str = st.nextToken("\n");
+                }
+                else{
+                    str = br.readLine();
+                }
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+
+	public static void main(String[] args) {
+		
+
+		try {
+			System.setIn(new FileInputStream("input.txt"));
+			System.setOut(new PrintStream(new FileOutputStream("output.txt")));
+		} catch (Exception e) {
+			System.err.println("Error");
+		}
+		FastReader sc = new FastReader();
+		
+        int n = sc.nextInt();
+        int [] arr = new int [n];
+
+        int nuts = 0;
+        for(int i=0; i<n; i++){
+            arr[i] = sc.nextInt();
+            if(arr[i] == 1) nuts++;
+        }
+
+        if(nuts == 0) System.out.println(0);
+
+        else{
+            long ans = 1;
+            Stack<Integer> st = new Stack<Integer>();
+            for(int i=0; i<n; i++){
+                if(arr[i] == 1){
+                    if(!st.isEmpty()){
+                        ans *= (i - st.peek());
+                    }
+                    st.push(i);
+                }
+            }
+            System.out.println(ans);
+        }
+
+
+	}
+}
